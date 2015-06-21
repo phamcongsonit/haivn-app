bed0096434ab697bedee494894c2fc092591b957
Pham Cong Son <phamcongsonit@gmail.com>
2015-06-21 20:05:55 +0700

    Update 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(function(){
+	$("#message").text("Đang tải dữ liệu, vui lòng đợi trong giây lát");
 	if (hasInternets()){
 		$("#webmobile").show();
 		$("#offline").hide();
 	}else{
 		$("#webmobile").hide();
+		$("#offline").text("Vui lòng kết nối mạng, hoặc server đang bảo trì");
 		$("#offline").show();
 	}
 });
